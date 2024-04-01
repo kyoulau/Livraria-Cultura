eb38003976c429d79ea1c0a5e9fb1f0d909ec815
andretini <andrealijaramaosagostini@gmail.com>
2024-04-01 14:56:34 -0300

    adição da pagina de perfil

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { PerfilComponent } from './perfil/perfil.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'perfil', component: PerfilComponent}
+
+];
